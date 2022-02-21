7f171347fd6414299d4eb2b4913fe9e649494399
Blackoutburst <blackoutburst@hotmail.com>
2022-02-21 16:29:41 +0100

    Added basic NMS file

---
+++ src/com/blackoutburst/bossbarapi/NMS.java
@@ -0,0 +1,40 @@
+package com.blackoutburst.bossbarapi;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import java.lang.reflect.Field;
+
+public class NMS {
+
+    public static Class<?> getClass(String name) {
+        try {
+            return Class.forName("net.minecraft.server." + Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3] + "." + name);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void sendPacket(Player player, Object packet) {
+        try {
+            final Object handle = player.getClass().getMethod("getHandle").invoke(player);
+            final Object playerConnection = handle.getClass().getField("playerConnection").get(handle);
+
+            playerConnection.getClass().getMethod("sendPacket", getClass("Packet")).invoke(playerConnection, packet);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void setField(Object edit, String fieldName, Object value) {
+        try {
+            final Field field = edit.getClass().getDeclaredField(fieldName);
+
+            field.setAccessible(true);
+            field.set(edit, value);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+}
